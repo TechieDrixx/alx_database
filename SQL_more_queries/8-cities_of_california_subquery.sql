d25017ab50c94ce5fc3750abfaaeebef5443609a
TechieDrixx <kaydee12000@gmail.com>
2023-08-19 13:58:51 -0700

    script

--- SQL_more_queries/8-cities_of_california_subquery.sql
+++ SQL_more_queries/8-cities_of_california_subquery.sql
@@ -1,12 +1,5 @@
--- Use the hbtn_0d_usa database
-USE hbtn_0d_usa;
-
--- Select cities of California using a subquery
-SELECT id, name
-FROM cities
-WHERE state_id = (
-    SELECT id
-    FROM states
-    WHERE name = 'California'
-)
-ORDER BY id ASC;
+SELECT cities.id, cities.name
+FROM cities, states
+WHERE cities.state_id = states.id
+AND states.name = 'California'
+ORDER BY cities.id ASC;
