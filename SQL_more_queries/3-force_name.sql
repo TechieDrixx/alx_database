04c93a08afd022267e46b7e9caf70a92e467b9df
TechieDrixx <kaydee12000@gmail.com>
2023-08-19 14:04:12 -0700

    script

--- SQL_more_queries/3-force_name.sql
+++ SQL_more_queries/3-force_name.sql
@@ -1,5 +1,6 @@
 -- Create the table force_name if not exists
 CREATE TABLE IF NOT EXISTS force_name (
-    id INT PRIMARY KEY,
-    name VARCHAR(256) NOT NULL
+    id INT,
+    name VARCHAR(256) NOT NULL,
+    PRIMARY KEY (id)
 );
